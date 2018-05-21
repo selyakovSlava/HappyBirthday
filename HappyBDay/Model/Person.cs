1e841c36824519228e7b86ccf62d820c3dfc2313
Slava <selyakov.vyacheslav@gmail.com>
2018-05-21 10:48:01 +0300

    Добавил файлы в репозиторий

---
+++ HappyBDay/Model/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HappyBDay.Model
+{
+    /// <summary>
+    /// Модель персоны.
+    /// </summary>
+    public class Person : INotifyPropertyChanged
+    {
+        private int _id; // Идентификационный номер.
+        private string _surname; // Фамилия.
+        private string _name; // Имя.
+        private string _patronymic; // Отчество.
+        private DateTime _bDay; // Дата рождения.
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Person() { }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="surname"></param>
+        /// <param name="name"></param>
+        /// <param name="patronymic"></param>
+        /// <param name="date"></param>
+        public Person(int id, string surname, string name, string patronymic, DateTime date)
+        {
+            this.ID = id;
+            this.Surname = surname;
+            this.Name = name;
+            this.Patronymic = patronymic;
+            this.BDay = date;
+        }
+
+        /// <summary>
+        /// Номер.
+        /// </summary>
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged("ID"); }
+        }
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; OnPropertyChanged("Surname"); }
+        }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged("Name"); }
+        }
+
+        /// <summary>
+        /// Отчество.
+        /// </summary>
+        public string Patronymic
+        {
+            get { return _patronymic; }
+            set { _patronymic = value; OnPropertyChanged("Patronymic"); }
+        }
+
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
+        public DateTime BDay
+        {
+            get { return _bDay; }
+            set { _bDay = value; OnPropertyChanged("BDay"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ HappyBDay/ViewModel/Persons.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HappyBDay.ViewModel
+{
+    public class Persons : INotifyPropertyChanged
+    {
+        private Model.Person _selectedPerson;
+        
+        /// <summary>
+        /// Список людей.
+        /// </summary>
+        public ObservableCollection<Model.Person> People { get; set; }
+
+        /// <summary>
+        /// Выбранная персона.
+        /// </summary>
+        public Model.Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set { _selectedPerson = value; OnPropertyChanged("SelectedPerson"); }
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Persons()
+        {
+            People = new ObservableCollection<Model.Person>();
+            LoadPersons();
+        }
+
+        /// <summary>
+        /// Загрузка списка людей.
+        /// </summary>
+        public void LoadPersons()
+        {
+            People.Add(new Model.Person(1, "Иванов", "Иван", "Иванович", new DateTime(1992, 4, 23)));
+            People.Add(new Model.Person(2, "Петров", "Петя", "Петрович", new DateTime(1991, 6, 6)));
+            People.Add(new Model.Person(3, "Сидоров", "Сидр", "Сидорович", new DateTime(1993, 5, 25)));
+        }
+
+        
+        private RelayCommand _addCommand; // команда добавления нового объекта
+        /// <summary>
+        /// Команда на добавление.
+        /// </summary>
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return _addCommand ??
+                  (_addCommand = new RelayCommand(obj =>
+                  {
+                      Model.Person person = new Model.Person();
+                      People.Insert(0, person);
+                      SelectedPerson = person;
+                  }));
+            }
+        }
+
+        private RelayCommand _removeCommand;
+        /// <summary>
+        /// Команда на удаление.
+        /// </summary>
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return _removeCommand ??
+                    (_removeCommand = new RelayCommand(obj =>
+                    {
+                        Model.Person person = obj as Model.Person;
+                        if (person != null)
+                        {
+                            People.Remove(person);
+                        }
+                    },
+                    (obj) => People.Count > 0));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
